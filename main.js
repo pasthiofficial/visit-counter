472d9468d1e50fb8ee92154b4dbd7fea8f2d9aa1
Linksopener <88886749+Linksopener@users.noreply.github.com>
2021-12-13 17:34:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const countEl = document.getElementById('count');
 updateVisitCount();
 
 function updateVisitCount() {
-	fetch('https://api.countapi.xyz/hit/linksopener.github.iovisit-counter/awesomeclick')
+	fetch('https://api.countapi.xyz/hit/linksopener.github.io/visit-counter/awesomeclick')
 	.then(res => res.json())
 	.then(res => {
 		countEl.innerHTML = res.value;
